beb14ef132a8881e009db92df200a9142c91b752
yh2n <yohann.potico@gmail.com>
2022-12-21 23:21:35 -0500

    Edit

--- Lesson_19/main.cpp
+++ Lesson_19/main.cpp
@@ -9,19 +9,17 @@ int main(int argc, const char * argv[]) {
     double a_double = 0.0;
 
     cout << endl <<
-        "Memory lego bricks, int: " << sizeof(an_int) << endl <<
-        "Memory lego bricks, float: " << sizeof(a_float) << endl <<
+        "Memory lego bricks, int:    " << sizeof(an_int) << endl <<
+        "Memory lego bricks, float:  " << sizeof(a_float) << endl <<
         "Memory lego bricks, double: " << sizeof(a_double) << endl;
 
     cout.setf(ios::hex);
     cout.unsetf(ios::dec);
     cout << endl <<
-        "int's address in memory is : " << &an_int << endl <<
-        "float's address in memory is : " << &a_float << endl <<
+        "int's address in memory is :    " << &an_int << endl <<
+        "float's address in memory is :  " << &a_float << endl <<
         "double's address in memory is : " << &a_double << endl;
 
-
-    
 
     return 0;
 }
