f94db553a0137687afc3c7689a7d0797ac96b648
yh2n <yohann.potico@gmail.com>
2023-01-10 12:32:48 -0500

    Bond class part1

---
+++ Lesson_27/Bond.h
@@ -0,0 +1,10 @@
+class Bond {
+    public:
+         Bond();
+
+         double principal;
+         double rate;
+         double years_to_maturity;
+         double payment_type;
+
+};
---
+++ Lesson_27/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Bond.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    Bond basic_bond;
+
+    cout << "Basic_bond.principal: " << basic_bond.principal << endl;
+
+    return 0;
+}
