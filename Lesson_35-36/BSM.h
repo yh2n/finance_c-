0ae05204f29e07148081e1213c3103074db1f047
yh2n <yohann.potico@gmail.com>
2023-02-09 15:16:38 -0500

    Black Scholes Monte Carlo Engine pI, pII

---
+++ Lesson_35-36/BSM.h
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class BSM {
+    public: 
+        BSM(float, float, float, float, float, long, long); 
+
+        ~BSM() {} 
+
+        float getBsmAsset();
+        float getBsmStrike();
+        float getBsmGrowth();
+        float getBsmVol();
+        float getBsmYears();
+        long getBsmSteps();
+        long getBsmMonteCarloSims();
+
+        void logNormalRandomWalk();
+
+        double getCallPrice();
+        double getPutPrice();
+
+        double rn(); 
+
+    private: 
+        float bsm_asset;
+        float bsm_strike;
+        float bsm_growth;
+        float bsm_vol;
+        float bsm_years;
+        long bsm_steps;
+        long bsm_monte_carlo_sims;
+
+        double bsm_call_price;
+        double bsm_put_price;
+}
